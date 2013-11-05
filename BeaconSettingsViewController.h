b2a136b8d1d2d074933d99ea1ab04a3b46fe41bf
David <jdavidcrow@gmail.com>
2013-11-05 11:44:11 -0800

    hooked up beacon settings UI items

--- BeaconSettingsViewController.h
+++ BeaconSettingsViewController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface BeaconSettingsViewController : UITableViewController
+@property (strong, nonatomic) IBOutlet UILabel *monitorLabel;
+@property (strong, nonatomic) IBOutlet UISwitch *monitorSwitch;
+@property (strong, nonatomic) IBOutlet UILabel *noteEntryLabel;
+@property (strong, nonatomic) IBOutlet UISwitch *noteEntrySwitch;
+@property (strong, nonatomic) IBOutlet UILabel *noteExitLabel;
+@property (strong, nonatomic) IBOutlet UISwitch *noteExitSwitch;
+@property (strong, nonatomic) IBOutlet UILabel *noteEntryOnDisplayLabel;
+@property (strong, nonatomic) IBOutlet UISwitch *noteEntryOnDisplaySwitch;
 
 @end
